9ae834df8c68402ff663789a342a5db4d4925d25
Edward <edward.girling@gmail.com>
2020-10-14 15:43:22 -0700

    changed the html for the site.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*dug*)j)xj7h(5aqd#4+g*13@tj4x9!=+w=vn&5s(va)9ohj$+'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
